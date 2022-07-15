afeaeacb9edb1c4ad7a7bbf67c568a806d91fad5
musaubrian <musaubrian15@gmail.com>
2022-07-16 01:15:08 +0300

    initialize bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of the array to be sorted
+ *
+ * Return: None
+ */
+void bubble_sort(int *array, size_t size)
+{}
